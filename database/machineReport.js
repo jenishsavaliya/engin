4f7ca5602db0e542e22f5e199e5a46d026c8da47
jay vaddoriya <jayvaddoriya06@gmail.com>
2023-10-04 16:30:05 +0530

    minor change

--- database/machineReport.js
+++ database/machineReport.js
@@ -28,7 +28,7 @@ module.exports = (sequelize, DataType) => {
         },
       },
     },
-    equipmentNamer: {
+    equipmentName: {
       type: DataType.STRING,
       default: "NULL",
       validate: {
